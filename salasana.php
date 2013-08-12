083fe0095b15b56d0b9d132e27579d467d7e669a
pturunen <pturunen@ovi.com>
2013-08-12 19:57:12 +0300

    kuva ja salasana.php

--- salasana.php
+++ salasana.php
@@ -3,7 +3,7 @@ $tunnus = $_POST["tunnus"];
 $salasana = $_POST["salasana"];
 if ($tunnus == "pallero" && $salasana == "opensource") {
 	echo "Tervetuloa kirjaamaan!";
-	echo "<img src="mustaherukka.jpg"  alt="LightenYourLife" >";
+	echo "<img src='mustaherukka.jpg' >";
 } else {
 	echo "Tarkista salasanasi!";
 }
