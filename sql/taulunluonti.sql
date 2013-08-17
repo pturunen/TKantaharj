05f96dee76a8bb8ef1d7c9ad9ee570e5dbbd8959
pturunen <pturunen@ovi.com>
2013-08-17 18:51:06 +0300

    rekisteri taulun luonti lisatty

--- sql/taulunluonti.sql
+++ sql/taulunluonti.sql
@@ -3,7 +3,7 @@ CREATE TABLE rekisteri (
   tunnus varchar(10) PRIMARY KEY,
   salasana varchar(18),
   luontipvm date,
-  sukupuoli vachar(1),
+  sukupuoli varchar(1),
   pituus int,
   paino int,
   ika int
