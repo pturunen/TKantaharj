8feef731d0e831ddcbfa553cf20589de67af93a3
pturunen <pturunen@ovi.com>
2013-08-17 19:45:31 +0300

    taulujen luonti ja taulujen poisto

--- sql/taulunluonti.sql
+++ sql/taulunluonti.sql
@@ -9,7 +9,43 @@ CREATE TABLE rekisteri (
   ika int
 );
 
+-- paivakirjan taulun luonti
+CREATE TABLE tapahtumapaiva (
+  tunnus varchar(10) PRIMARY KEY REFERENCES rekisteri(tunnus) ON DELETE CASCADE,
+  paiva date PRIMARY KEY,
+  paino int,
+  selite varchar(50)
+);
 
+-- raakaaine taulun luonti
+CREATE TABLE raakaaine (
+  nimi varchar(50) PRIMARY KEY,
+  valmistaja varchar PRIMARY KEY,
+  luokka varchar,
+  selite varchar(50)
+);
 
+-- paivakirjan rivin luonti
+CREATE TABLE energiansaanti (
+  id serial  PRIMARY KEY REFERENCES rekisteri(tunnus) ON DELETE CASCADE,
+  paiva date REFERENCES tapahtumapaiva(paiva) ON DELETE CASCADE,
+  ruoka varchar(40) REFERENCES raakaaine(nimi) ON DELETE CASCADE,
+  valmistaja varchar REFERENCES raakaaine(valmistaja) ON DELETE CASCADE,
+  maara integer
+);
 
+-- perusravintoaine taulun luonti
+CREATE TABLE perusravintoaineet (
+  id serial  PRIMARY KEY,
+  nimi varchar(40) REFERENCES raakaaine(nimi) ON DELETE CASCADE,
+  mittayksikko varchar(4),
+  maara integer
+);
 
+-- kivennaisjahivenaineet taulun luonti
+CREATE TABLE kivhivenaineet (
+  id serial  PRIMARY KEY,
+  nimi varchar(40) REFERENCES raakaaine(nimi) ON DELETE CASCADE,
+  mittayksikko varchar(4),
+  maara integer
+);
--- sql/taulunpoisto.sql
+++ sql/taulunpoisto.sql
@@ -1,5 +1,10 @@
--- Kayttaja-taulun luonti
+-- lightenyourlife taulujen poisto
 DROP TABLE rekisteri;
+DROP TABLE tapahtumapaiva;
+DROP TABLE raakaaine;
+DROP TABLE energiansaanti;
+DROP TABLE perusravintoaineet;
+DROP TABLE kivhivenaineet;
 
 
 
