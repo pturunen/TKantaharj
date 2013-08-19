cd5f647c9345301b094d9a7d5150e6d0b8a4e114
pturunen <pturunen@ovi.com>
2013-08-19 21:59:10 +0300

    hakemistokokeilu

---
+++ yhteys/kirjautuminen.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'yhteys/yhteys.php';
+
+
+if (isset($_GET['sisaan'])) {
+  $kayttaja = $kyselija->tunnista($_POST['tunnus'], $_POST['salasana']);
+  if ($kayttaja) {
+    $sessio->kayttaja_id = $kayttaja->id;
+	echo "<p> tunnarit tunnistettu</p> "
+    ohjaa('haku.php');
+  } else {
+    ohjaa('etusivu.php');
+  }
+} elseif (isset($_GET['ulos'])) {
+  unset($sessio->kayttaja_id);
+  ohjaa('haku.php');
+} else {
+  die('Laiton toiminto!');
+}
+
