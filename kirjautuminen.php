282f424fe5f9bf109415567f960057d26594a482
pturunen <pturunen@ovi.com>
2013-08-20 00:23:28 +0300

    kirjautuminen header

--- kirjautuminen.php
+++ kirjautuminen.php
@@ -6,13 +6,13 @@ if (isset($_GET['sisaan'])) {
   $kayttaja = $kyselija->tunnista($_POST['tunnus'], $_POST['salasana']);
   if ($kayttaja) {
     $sessio->kayttaja_id = $kayttaja->id;
-    header('haku.php');
+    header("Location: haku.php");
   } else {
-    header('etusivu.php');
+    header("Location: etusivu.php");
   }
 } elseif (isset($_GET['ulos'])) {
   unset($sessio->kayttaja_id);
-  header('haku.php');
+  header("Location: haku.php");
 } else {
   die('Laiton toiminto!');
 }
