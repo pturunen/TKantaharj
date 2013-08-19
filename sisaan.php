4d3bfed5c3261a434968a8f04653e25bfc2d6aec
pturunen <pturunen@ovi.com>
2013-08-20 02:31:38 +0300

    opettelua puhtaalta poydalta

--- sisaan.php
+++ sisaan.php
@@ -1,20 +1,27 @@
 <?php
 session_start();
 
-$salasanat = array();
-$salasanat["aapeli"] = "kissa";
-$salasanat["maija"] = "pupu";
-
-$tunnus = $_POST["tunnus"];
-$salasana = $_POST["salasana"];
-
-if (isset($salasanat[$tunnus])) {
-    if ($salasanat[$tunnus] == $salasana) {
-        $_SESSION["kayttaja"] = $tunnus;
-        header("Location: sisalto.php");
-        die();
-    }
+// yhteyden muodostus tietokantaan
+try {
+    $yhteys = new PDO("pgsql:host=localhost;dbname=pcturune",
+                      "pcturune", "42c747d22fbafe6e");
+} catch (PDOException $e) {
+    die("VIRHE: " . $e->getMessage());
 }
+$yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if (isset($_POST['tunnus'])) {
+    $kysely = $yhteys->prepare('SELECT * FROM rekisteri WHERE tunnus = ? and password = ?');
+    $kysely->execute(array($_POST["tunnus"], $_POST["password"]));
+	$kayttaja = $kysely->fetchObject();
+	if ($kayttaja) {
+		$_SESSION["kayttaja_id"] = $kayttaja->id;
+		$_SESSION["kayttaja"] = $_POST["tunnus"];
+		header("Location: sisalto.php");
+		die();
+		} 
+} 
+
 ?>
 <p>Tunnus tai salasana on väärin!</p>
 <p><a href="eka.html">Takaisin</a></p>
