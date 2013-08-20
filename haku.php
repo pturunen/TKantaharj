ddbb6265254a38e628540a1408e685ca0b85bac6
pturunen <pturunen@ovi.com>
2013-08-20 20:47:03 +0300

    lisatty haku.php ketjuun

--- haku.php
+++ haku.php
@@ -1,33 +1,4 @@
 <?php
-session_start();
-
-// yhteyden muodostus tietokantaan
-try {
-    $yhteys = new PDO("pgsql:host=localhost;dbname=pcturune",
-                      "pcturune", "42c747d22fbafe6e");
-} catch (PDOException $e) {
-    die("VIRHE: " . $e->getMessage());
-}
-$yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// kyselyn suoritus   
-//  if (isset($_POST["nimi"])) {
-$kysely = $yhteys->prepare("SELECT * FROM raakaaine WHERE nimi = 'rasvaton maito'");
-
-//$kysely->execute($_POST["nimi"]);
-$kysely->execute();
-// haettujen rivien tulostus
-echo "<table border>";
-while ($rivi = $kysely->fetch()) {
-    echo "<tr>";
-    echo "<td>" . " roskaa" . $rivi["nimi"] . "</td>";
-    echo "<td>" . $rivi["valmistaja"] . "</td>";
-	echo "<td>" . $rivi["luokka"] . "</td>";
-	echo "<td>" . $rivi["selite"] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
-//}
+header("Location: satunnainen.html");
+die();
 ?>
-
-<p><a href="eka.html">Takaisin</a></p>
