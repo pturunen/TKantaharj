d3986160abe3a5fc3dc56493bba78965e0913619
pturunen <pturunen@ovi.com>
2013-08-20 23:48:48 +0300

    jokerimerkki kyselyyn

--- satunnainen.php
+++ satunnainen.php
@@ -10,7 +10,7 @@ try {
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_POST['nimi'])) {
-    $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi LIKE  ? ');
+    $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi LIKE  '%maito%'');
     $kysely->execute(array($_POST[%nimi%"]));
 		echo "<table border>";
 		while ($rivi = $kysely->fetch()) {
