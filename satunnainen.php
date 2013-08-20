ca3bbfd3d2bf5133b25ba57de3cb43b4a4a7dcb3
pturunen <pturunen@ovi.com>
2013-08-20 23:01:49 +0300

    poistettu printl lauseet

--- satunnainen.php
+++ satunnainen.php
@@ -9,17 +9,11 @@ try {
     die("VIRHE: " . $e->getMessage());
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "ennen kyselya";
 if (isset($_POST['nimi'])) {
-echo "kysely ";
     $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi = ?');
     $kysely->execute(array($_POST["nimi"]));
-   //$kysely->execute();
-	//$kayttaja = $kysely->fetchObject();
-	echo "alkaa tulostus rivi kerrallaan";
 		echo "<table border>";
 		while ($rivi = $kysely->fetch()) {
-		echo "while lauseen sisalla";
 			echo "<tr>";
 			echo "<td>" . $rivi["nimi"] . "</td>";
 			echo "<td>" . $rivi["valmistaja"] . "</td>";
@@ -27,7 +21,6 @@ echo "kysely ";
 			echo "<td>" . $rivi["selite"] . "</td>";
 			echo "</tr>";
 		}
-		echo "while lauseen ulkona lopussa";
 		echo "</table>";
 	//die();
 } 
