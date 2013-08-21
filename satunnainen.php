bdfead73fc55b9e9a7220c2683a0955606249557
pturunen <pturunen@ovi.com>
2013-08-21 14:54:16 +0300

    linkki

--- satunnainen.php
+++ satunnainen.php
@@ -25,7 +25,8 @@ if (isset($_POST['nimi'])) {
 			echo "<td>" . $rivi["luokka"] . "</td>";
 			echo "<td>" . $rivi["selite"] . "</td>";
 			echo "</tr>";
-			
+			Echo "<a href=alitaulut.php>$rivi["nimi"]</a>"
+
 			//<p><a href="alitaulut.php"> <?" " . $rivi["nimi"] . " " . $rivi["valmistaja"] . " " . $rivi["luokka"] . " " . $rivi["selite"] . " "</a></p>
 	
 		}
