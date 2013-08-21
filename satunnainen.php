de1047650cd7252c3bf5c8e290cd65726f4a853a
pturunen <pturunen@ovi.com>
2013-08-21 15:52:58 +0300

    linkki

--- satunnainen.php
+++ satunnainen.php
@@ -29,7 +29,7 @@ if (isset($_POST['nimi'])) {
 			$muuttuja2 = $rivi["valmistaja"];
 			//$kokorivi = "{$muuttuja} {$muuttuja2}";
 			//echo "<a href=alitaulut.php>$kokorivi</a>";
-			echo "<a href="alitaulut.php">$rivi["nimi"]</a>"
+			echo "<a href=\"alitaulut.php\">$rivi["nimi"]</a>"
 
 			//<p><a href="alitaulut.php"> <?" " . $rivi["nimi"] . " " . $rivi["valmistaja"] . " " . $rivi["luokka"] . " " . $rivi["selite"] . " "</a></p>
 	
