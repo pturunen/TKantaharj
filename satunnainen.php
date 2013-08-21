0dd3c7f957c59d7afa1411e832da8275ba66ee76
pturunen <pturunen@ovi.com>
2013-08-21 14:27:46 +0300

    paluu

--- satunnainen.php
+++ satunnainen.php
@@ -13,29 +13,24 @@ if (isset($_POST['nimi'])) {
     
     $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi LIKE  ?');
     $kysely->execute(array("%". $_POST['nimi'] . "%"));
-	?>
-	<h1> Ruoka-aineet : </h1>
-	<ul>
-		<!--//echo "<table border>";
+	
+		echo "<table border>";
 		//<form action ="alitaulut.php" method="post">
-		-->
-		<? while ($rivi = $kysely->fetch()) {?>
-		<li>
-		<!-- //   <input type="submit" value=""<td>" . $rivi["nimi"] . "</td>" . "<td>" . $rivi["valmistaja"] . "</td>" . "<td>" . $rivi["luokka"] . "</td>" . "<td>" . $rivi["selite"] . "</td>"";
+		while ($rivi = $kysely->fetch()) {
+		//   <input type="submit" value=""<td>" . $rivi["nimi"] . "</td>" . "<td>" . $rivi["valmistaja"] . "</td>" . "<td>" . $rivi["luokka"] . "</td>" . "<td>" . $rivi["selite"] . "</td>"";
 			
-			/*echo "<tr>";
+			echo "<tr>";
 			echo "<td>" . $rivi["nimi"] . "</td>";
 			echo "<td>" . $rivi["valmistaja"] . "</td>";
 			echo "<td>" . $rivi["luokka"] . "</td>";
 			echo "<td>" . $rivi["selite"] . "</td>";
 			echo "</tr>";
-			*/
-			-->
-			<p><a href="alitaulut.php"> <?" " . $rivi["nimi"] . " " . $rivi["valmistaja"] . " " . $rivi["luokka"] . " " . $rivi["selite"] . "?> "</a></p>
-		</li>
-		<?}?>
-		//</form>
-		//echo "</table>";
+			
+			//<p><a href="alitaulut.php"> <?" " . $rivi["nimi"] . " " . $rivi["valmistaja"] . " " . $rivi["luokka"] . " " . $rivi["selite"] . "?> "</a></p>
+	
+		}
+		</form>
+		echo "</table>";
 	</ul>
 } 
 
