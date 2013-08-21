387a741ee0fc8c5b0e2ef05f8173b75be277a19b
pturunen <pturunen@ovi.com>
2013-08-21 20:14:48 +0300

    header Location ei toimi lisatty kenoviivat

--- satunnainen.php
+++ satunnainen.php
@@ -8,9 +8,9 @@ try {
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_POST['nimi'])){
-header("Location: satunnainen.html");
     $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi LIKE  ?');
     $kysely->execute(array("%". $_POST['nimi'] . "%"));
+	header("Location: satunnainen.html");
 	if (!$kysely){
 	header("Location: satunnainen.html");
 	die();
