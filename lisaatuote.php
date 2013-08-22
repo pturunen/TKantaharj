415e2cab0c982fd298a022f663d323d4e2376194
pturunen <pturunen@ovi.com>
2013-08-22 17:45:54 +0300

    tuotteen lisays kayttajana

---
+++ lisaatuote.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if (!isset($_SESSION["kayttaja"])) {
+    header("Location: eka.html");
+    die();
+}
+// yhteyden muodostus tietokantaan
+try {
+    $yhteys = new PDO("pgsql:host=localhost;dbname=pcturune",
+                      "pcturune", "42c747d22fbafe6e");
+} catch (PDOException $e) {
+    die("VIRHE: " . $e->getMessage());
+}
+$yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if (isset($_POST['nimi'])) {
+    $kysely = $yhteys->prepare('INSERT INTO raakaaine (nimi,valmistaja,luokka,selite) VALUES (?,?,?,?)');
+    $onnistuiko = $kysely->execute(array($_POST["nimi"], $_POST["valmistaja"],$_POST["luokka"],$_POST["selite"]));
+	if ($onnistuiko) {
+		header("Location: satunnainenkavija.php");
+		die();
+		} 
+} 
+
+?>
+<p>Tunnus tai salasana on väärin!</p>
+<p><a href="eka.html">Takaisin</a></p>
