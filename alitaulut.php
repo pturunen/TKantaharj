d030c1d21011f32948097bbc548f3cc9994b65d3
pturunen <pturunen@ovi.com>
2013-08-22 13:05:51 +0300

    alitaulun muokkaus

--- alitaulut.php
+++ alitaulut.php
@@ -11,26 +11,21 @@ try {
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_POST['nimi'])) {
     
-    $kysely = $yhteys->prepare('SELECT * FROM raakaaine WHERE nimi LIKE  ?');
-    $kysely->execute(array("%". $_POST['nimi'] . "%"));
-
+    $kysely = $yhteys->prepare('SELECT * FROM perusravintoaineet WHERE nimi = ?');
+    $kysely->execute(array($_POST['nimi']));
 		echo "<table border>";
-		<form action ="alitaulut.php" method="post">
-		
 		while ($rivi = $kysely->fetch()) {
-		    <input type="submit" value=""<td>" . $rivi["nimi"] . "</td>" . "<td>" . $rivi["valmistaja"] . "</td>" . "<td>" . $rivi["luokka"] . "</td>" . "<td>" . $rivi["selite"] . "</td>""
-			/*echo "<tr>";
+			echo "<tr>";
 			echo "<td>" . $rivi["nimi"] . "</td>";
-			echo "<td>" . $rivi["valmistaja"] . "</td>";
-			echo "<td>" . $rivi["luokka"] . "</td>";
-			echo "<td>" . $rivi["selite"] . "</td>";
-			echo "</tr>";*/
+			echo "<td>" . $rivi["ravintotekija"] . "</td>";
+			echo "<td>" . $rivi["maara"] . "</td>";
+			echo "<td>" . $rivi["mittayksikko"] . "</td>";
+			echo "</tr>";
 		}
-		</form>
 		echo "</table>";
 } 
-
 ?>
+<p><a href="satunnainen.php">Takaisin listaukseen</a></p>
 <p><a href="satunnainen.html">Tuotehakuun takaisin</a></p>
 <p><a href="eka.html">Takaisin etusivulle</a></p>
 
