9425d3955b5e26d315f8e541a0bc0a812bfb89ca
pturunen <pturunen@ovi.com>
2013-08-23 20:52:11 +0300

    korjattu female -> f ja male -> m

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -13,10 +13,13 @@ $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //$aika = $yhteys->prepare('SELECT CURRENT_DATE AS n;');
 //$aika->execute();
 //$kannanpvm = $aika->fetch();
-
+$genre = 'm';
 if (isset($_POST['tunnus']) && isset($_POST['salasana'])) {
+    if ($_POST['sukupuoli'] == 'nainen'){
+	$genre = 'f';
+	}
     $kysely = $yhteys->prepare('INSERT INTO rekisteri (tunnus,salasana,luontipvm,sukupuoli,pituus,paino,ika) VALUES (?,?,?,?,?,?,?)');
-    $onnistuiko = $kysely->execute(array($_POST["tunnus"], $_POST["salasana"],DATE(NOW()),$_POST["sukupuoli"],$_POST["pituus"],$_POST["paino"],$_POST["ika"]));
+    $onnistuiko = $kysely->execute(array($_POST["tunnus"], $_POST["salasana"],DATE(NOW()),$genre,$_POST["pituus"],$_POST["paino"],$_POST["ika"]));
 	if ($onnistuiko) {
 		$kysely = $yhteys->prepare('SELECT id FROM rekisteri WHERE tunnus = ? and salasana = ?');
 		$kysely->execute(array($_POST["tunnus"], $_POST["salasana"]));
