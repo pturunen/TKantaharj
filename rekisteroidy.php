875c9f974c7346776dd67db7a75512b9ffc5d78a
pturunen <pturunen@ovi.com>
2013-08-23 17:34:28 +0300

    muokattu date kyselya

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -8,7 +8,7 @@ try {
     die("VIRHE: " . $e->getMessage());
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$paivays = date('Y-n-j');
+$paivays = date('Y-m-d');
 //$paivays = date("j.n.Y");
 //$aika = $yhteys->prepare('SELECT CURRENT_DATE;');
 //$aika->execute();
