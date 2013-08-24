9f818c0d1dd37d5601f5d97e1fca4ce4acc258e3
pturunen <pturunen@ovi.com>
2013-08-24 23:05:48 +0300

    energian lasku

--- pkhaku.php
+++ pkhaku.php
@@ -62,7 +62,7 @@ catch (PDOException $e) {
 			echo "<td>" . $rivi["ruoka"] . "</td>";//energiansaanti ruoka where tapid == tapahtumapaiva.id
 			echo "<td>" . $rivi["emaara"] . "</td>"; //energiansaanti maara where tapid == tapahtumapaiva.id
 			echo "<td>" . $rivi["energia"] . "</td>"; //perusravintoaineet ravintotekija where ravintotekija== energia and nimi == energiansaanti.ruoka
-			$saatuenergia = ($rivi["pmaara"]\100)*$rivi["emaara"];
+			$saatuenergia = ($rivi["pmaara"]/100)*$rivi["emaara"];
 			echo "<td>" . $rivi["$saatuenergia"] . "</td>";//perusravintoaineet maara where ravintotekija == energia and nimi == energiansaanti.ruoka
 			echo "</tr>";
 			$rivi = $kysely->fetch();
