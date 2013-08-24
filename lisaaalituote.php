0c265c1a57a4e2fd47c8cf831b9673680d3480be
pturunen <pturunen@ovi.com>
2013-08-24 18:21:42 +0300

    sisalto sivun muokkaus

--- lisaaalituote.php
+++ lisaaalituote.php
@@ -4,7 +4,6 @@ if (!isset($_SESSION["kayttaja"])) {
     header("Location: eka.html");
     die();
 }
-
 // yhteyden muodostus tietokantaan
 try {
     $yhteys = new PDO("pgsql:host=localhost;dbname=pcturune",
@@ -14,74 +13,79 @@ try {
 }
 $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (isset($_POST['nimip'])  && isset($_POST['ravintotekijap']) ) {
+	try{
     $kysely = $yhteys->prepare('INSERT INTO perusravintoaineet (ravintotekija,nimi,maara,mittayksikko) VALUES (?,?,?,?)');
     $onnistuikop = $kysely->execute(array($_POST["ravintotekijap"], $_POST["nimip"],$_POST["maarap"],$_POST["mittayksikkop"]));
 	}
-	//if ($onnistuikop) {
-		$kysely = $yhteys->prepare('SELECT * FROM perusravintoaineet WHERE nimi =  ?');
-		$tulos = $kysely->execute(array($_POST['nimip']));
+	catch (PDOException $e) {
+    //echo "VIRHE: " . $e->getMessage();
+	}
+}
+try {
+	$kysely = $yhteys->prepare('SELECT * FROM perusravintoaineet WHERE nimi =  ?');
+	$tulos = $kysely->execute(array($_POST['nimip']));
+	$rivip = $kysely->fetch();
+	}
+	catch (PDOException $e) {
+    //echo "VIRHE: " . $e->getMessage();
+	}
+	echo "<table border>";
+	echo "<tr>";
+	echo "<td>" . "  ". "PERUSRAVINTOAINEET" . " " ."</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>" . "  ". "RAVINTOTEKIJÄ" . " " ."</td>";
+	echo "<td>" . "  ". "NIMI" . " " ."</td>";
+	echo "<td>" . "  ". "MÄÄRÄ" . " " ."</td>";
+	echo "<td>" . "  ". "MITTAYKSIKKÖ" . " " ."</td>";
+	echo "</tr>";
+	while ($rivip) {
+		echo "<tr>";
+		echo "<td>" . $rivip["ravintotekija"] . "</td>";
+		echo "<td>" . $rivip["nimi"] . "</td>";
+		echo "<td>" . $rivip["maara"] . "</td>";
+		echo "<td>" . $rivip["mittayksikko"] . "</td>";
+		echo "</tr>";
 		$rivip = $kysely->fetch();
-		echo "<table border>";
-		echo "<tr>";
-		echo "<td>" . "  ". "PERUSRAVINTOAINEET" . " " ."</td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo "<td>" . "  ". "RAVINTOTEKIJÄ" . " " ."</td>";
-		echo "<td>" . "  ". "NIMI" . " " ."</td>";
-		echo "<td>" . "  ". "MÄÄRÄ" . " " ."</td>";
-		echo "<td>" . "  ". "MITTAYKSIKKÖ" . " " ."</td>";
-		echo "</tr>";
-		while ($rivip) {
-			echo "<tr>";
-			echo "<td>" . $rivip["ravintotekija"] . "</td>";
-			echo "<td>" . $rivip["nimi"] . "</td>";
-			echo "<td>" . $rivip["maara"] . "</td>";
-			echo "<td>" . $rivip["mittayksikko"] . "</td>";
-			echo "</tr>";
-			$rivip = $kysely->fetch();
 		}
 		echo "</table>";
-	//	} 
-
  if (isset($_POST['nimik']) && isset($_POST['ravintotekijak']) ) {
     try{
     $kyselyk = $yhteys->prepare('INSERT INTO kivhivenaineet (ravintotekija,nimi,maara,mittayksikko) VALUES (?,?,?,?)');
     $onnistuikok = $kyselyk->execute(array($_POST["ravintotekijak"], $_POST["nimik"],$_POST["maarak"],$_POST["mittayksikkok"]));
 	}
 	catch (PDOException $e) {
-     echo "VIRHE: " . $e->getMessage();
+     //echo "VIRHE: " . $e->getMessage();
     }
+}
+try{
+	$kyselyk = $yhteys->prepare('SELECT * FROM kivhivenaineet WHERE nimi =  ?');
+	$tulos = $kyselyk->execute(array($_POST['nimik']));
+	$rivik = $kyselyk->fetch();
 	}
-	//if ($onnistuikok) {
-	try{
-		$kyselyk = $yhteys->prepare('SELECT * FROM kivhivenaineet WHERE nimi =  ?');
-		$tulos = $kyselyk->execute(array($_POST['nimik']));
+catch(PDOException $e) {
+    //echo "VIRHE: " . $e->getMessage();
+}
+	echo "<table border>";
+	echo "<tr>";
+	echo "<td>" . "  ". "KIVENNÄIS-JA HIVENAINEET" . " " ."</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>" . "  ". "RAVINTOTEKIJÄ" . " " ."</td>";
+	echo "<td>" . "  ". "NIMI" . " " ."</td>";
+	echo "<td>" . "  ". "MÄÄRÄ" . " " ."</td>";
+	echo "<td>" . "  ". "MITTAYKSIKKÖ" . " " ."</td>";
+	echo "</tr>";
+	while ($rivik) {
+		echo "<tr>";
+		echo "<td>" . $rivik["ravintotekija"] . "</td>";
+		echo "<td>" . $rivik["nimi"] . "</td>";
+		echo "<td>" . $rivik["maara"] . "</td>";
+		echo "<td>" . $rivik["mittayksikko"] . "</td>";
+		echo "</tr>";
 		$rivik = $kyselyk->fetch();
-		}
-	catch(PDOException $e) {
-     echo "VIRHE: " . $e->getMessage();
-    }
-		echo "<table border>";
-		echo "<tr>";
-		echo "<td>" . "  ". "KIVENNÄIS-JA HIVENAINEET" . " " ."</td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo "<td>" . "  ". "RAVINTOTEKIJÄ" . " " ."</td>";
-		echo "<td>" . "  ". "NIMI" . " " ."</td>";
-		echo "<td>" . "  ". "MÄÄRÄ" . " " ."</td>";
-		echo "<td>" . "  ". "MITTAYKSIKKÖ" . " " ."</td>";
-		echo "</tr>";
-		while ($rivik) {
-			echo "<tr>";
-			echo "<td>" . $rivik["ravintotekija"] . "</td>";
-			echo "<td>" . $rivik["nimi"] . "</td>";
-			echo "<td>" . $rivik["maara"] . "</td>";
-			echo "<td>" . $rivik["mittayksikko"] . "</td>";
-			echo "</tr>";
-			$rivik = $kyselyk->fetch();
-		}
-		echo "</table>";
-	//	} 
+	}
+	echo "</table>";
 ?>
 <p><a href="lisaaalituote2.php">Lisaa ravintoaineita</a></p>
 <p><a href="lisaatuote.html">Lisaa uusi tuote</a></p>
--- sisalto.php
+++ sisalto.php
@@ -5,7 +5,22 @@ if (!isset($_SESSION["kayttaja"])) {
     die();
 }
 ?>
-<p>Tervetuloa, <?php echo $_SESSION["kayttaja"]; ?>!</p>
+<head>
+    <style type="text/css">
+	body {background-color:#d0e4fe;}
+	h1 {
+	color:orange;
+	text-align:center;
+	}
+	p {color:blue;
+	font-size:15px
+	}
+	fieldset { font-size:12px }
+	</style>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Tervetuloa, <?php echo $_SESSION["kayttaja"]; ?>!</title>
+  </head>
+
 <p><a href="haku.php">Siirry tuotehakuun</a></p>
 <p><a href="lisaatuote.html">Lisaa uusi raaka-aine</a></p>
 <p><a href="ulos.php">Kirjaudu ulos</a></p>
