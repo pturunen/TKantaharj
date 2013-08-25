bb1c9d3fa4b6f837783b4f4d21accd5183078c18
pturunen <pturunen@ovi.com>
2013-08-25 16:59:52 +0300

    muokattu pk tapahtumien nayttoa

--- pkhaku.php
+++ pkhaku.php
@@ -21,12 +21,6 @@ else if (isset($_POST['aika']) && $_POST['aika'] == 'val'){
 		$haevali = true;
 }	
 try {
-   /* $kysely = $yhteys->prepare('SELECT tapahtumapaiva.id,tapahtumapaiva.paiva,tapahtumapaiva.paino,tapahtumapaiva.selite,
-	energiansaanti.ruoka, energiansaanti.maara, perusravintoaineet.maara
-	FROM tapahtumapaiva,raakaaine,energiansaanti,perusravintoaineet 
-	WHERE tunnus = ? && tunnus == tapahtumapaiva.tunnus && tapahtumapaiva.id == energiansaanti.tapid && 
-	energiansaanti.ruoka == perusravintoaineet.nimi');
-	*/
 	if ($haekaikki){
 	$kysely = $yhteys->prepare('SELECT tapahtumapaiva.id,tapahtumapaiva.paiva AS paiva,tapahtumapaiva.paino AS paino,tapahtumapaiva.selite AS seli,
 	energiansaanti.ruoka AS ruoka, energiansaanti.maara AS emaara, perusravintoaineet.maara as pmaara
@@ -61,7 +55,7 @@ catch (PDOException $e) {
 	//echo "Päiväkirjassa ei ole tapahtumia annettuna ajanjaksona! <br>";
 	}
 	else {
-		echo "<table border>";
+		/*echo "<table border>";
 		echo "<tr>";
 		echo "<td>" . "  ". "PÄIVÄKIRJA TAPAHTUMAT" . " " ."</td>";
 		echo "</tr>";
@@ -77,23 +71,82 @@ catch (PDOException $e) {
 		while ($rivi) {
 		   $saatuenergia = ($rivi["pmaara"]/100)*$rivi["emaara"];
 			echo "<tr>";
-			echo "<td>" . $rivi["paiva"] . "</td>";//tapahtumapaiva paiva where tunnus == $_SESSION['kayttaja'] and paiva == paivastart -paivaend
-			echo "<td>" . $rivi["paino"] . "</td>";//tapahtumapaiva selite where tunnus == $_SESSION['kayttaja']
-			echo "<td>" . $rivi["seli"] . "</td>"; //tapahtumapaiva selite where tunnus == $_SESSION['kayttaja']
-			echo "<td>" . $rivi["ruoka"] . "</td>";//energiansaanti ruoka where tapid == tapahtumapaiva.id
-			echo "<td>" . $rivi["emaara"] . "</td>"; //energiansaanti maara where tapid == tapahtumapaiva.id
-			echo "<td>" . "energia" . "</td>"; //perusravintoaineet ravintotekija where ravintotekija== energia and nimi == energiansaanti.ruoka
-			echo "<td>" . $saatuenergia . "</td>";//perusravintoaineet maara where ravintotekija == energia and nimi == energiansaanti.ruoka
+			echo "<td>" . $rivi["paiva"] . "</td>";
+			echo "<td>" . $rivi["paino"] . "</td>";
+			echo "<td>" . $rivi["seli"] . "</td>"; 
+			echo "<td>" . $rivi["ruoka"] . "</td>";
+			echo "<td>" . $rivi["emaara"] . "</td>"; 
+			echo "<td>" . "energia" . "</td>";
+			echo "<td>" . $saatuenergia . "</td>";
 			echo "</tr>";
 			$rivi = $kysely->fetch();
 		}
 		echo "</table>";
+		*/
+		?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+	body {background-color:#d0e4fe;}
+	title {
+	color:orange;
+	text-align:center;
 	}
+	p {color:blue;
+	font-size:15px
+	}
+	fieldset { font-size:12px }
+	</style>
+    <title>LightenYourLife paivakirja taphtumat</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body>
+    <img src="paivakirja.jpg" title="LightenYourLife" width="120" height="80" alt="LightenYourLife" />
+	<h1> Päiväkirja tapahtumat</h1>
+	<p></p>
+      <fieldset>
+        <legend>Päiväkirja tapahtumat</legend>
+		<table border>";
+		<tr>
+		<td>PÄIVÄKIRJA TAPAHTUMAT</td>
+		</tr>
+		<tr>
+		<td>PÄIVÄMÄÄRÄ</td>
+		<td>PAINO</td>
+		<td>SELITE</td>
+		<td>RUOKA-AINE</td>
+		<td>MÄÄRÄ</td>
+		<td>PERUSRAVINTOAINE</td>
+		<td>ENERGIAN SAANTI KJ</td>
+		</tr>
+		while ( <?php $rivi ?> ) {
+		   <?php $saatuenergia = <?php ($rivi["pmaara"]/100)*$rivi["emaara"];?>
+			<tr>
+			<td><?php $rivi["paiva"]?></td>
+			<td><?php $rivi["paino"]?> </td>
+			<td><?php $rivi["seli"]?></td>
+			<td><?php $rivi["ruoka"]?></td>
+			<td><?php $rivi["emaara"]?></td> 
+			<td>energia</td>
+			<td><?php $saatuenergia?></td>
+			</tr>
+			<?php $rivi = $kysely->fetch();?>
+		}
+		</table>
+		<br>
+      </fieldset>
+  </body>
+  </head>
+  </html>		
+<?php
+	}
+	
 	$nimiparametri = $rivi["nimi"];
 	if (isset($_SESSION["kayttaja"])) {
-    echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"lisaaalituote2.php\">Lisaa ravintoaineelle lisätietoja <br></a>";
+    //echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"lisaaalituote2.php\">Lisaa ravintoaineelle lisätietoja <br></a>";
 	echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"paivakirja.php\">edellinen sivu <br></a>";
-    echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"lisaatuote.html\">Lisaa uusi tuote<br></a>";
+    //echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"lisaatuote.html\">Lisaa uusi tuote<br></a>";
 	echo "<a border-style:\"solid\" style=\"color: blue\"  href=\"ulos.php\">Kirjaudu ulos<br></a>";
 	}
 
