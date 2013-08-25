04a9212272815ec206b31bc1c0c683ab228f9503
pturunen <pturunen@ovi.com>
2013-08-25 22:52:06 +0300

    paivakirja lisays

--- pklisays.php
+++ pklisays.php
@@ -73,10 +73,9 @@ echo "s post paiva on jo tyhja";
 
 //tapahtumapaivan rivien paivitys naytolle joka kerta
 if (isset($_POST['paiva']) || isset($_SESSION['lisayspaiva'])){
-	if (!isset($_POST['paiva'])){
-	$_POST['paiva'] = $_SESSION['lisayspaiva'];
-	}
+	if (!empty($_POST['paiva'])){
 	$_SESSION['lisayspaiva'] = $_POST['paiva'];
+	}
 	try {
 $kysely = $yhteys->prepare('SELECT tapahtumapaiva.id,tapahtumapaiva.paiva AS paiva,tapahtumapaiva.paino AS paino,tapahtumapaiva.selite AS seli,
 	energiansaanti.ruoka AS ruoka, energiansaanti.maara AS emaara, perusravintoaineet.maara as pmaara
