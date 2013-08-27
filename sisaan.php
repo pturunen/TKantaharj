893858b005755523748f94fe3c9d46e31fa326b2
pturunen <pturunen@ovi.com>
2013-08-27 19:31:25 +0300

    korjattu tunnuksen ja salarin luontia

--- sisaan.php
+++ sisaan.php
@@ -12,9 +12,14 @@ $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if (isset($_POST['tunnus']) && !empty($_POST['tunnus']) && !empty($_POST['salasana'])) {
     if (strlen($_POST['tunnus']) > 0 && strlen($_POST['salasana']) > 0){
+try {	
     $kysely = $yhteys->prepare('SELECT * FROM rekisteri WHERE tunnus = ? and salasana = ?');
     $kysely->execute(array($_POST["tunnus"], $_POST["salasana"]));
 	$kayttaja = $kysely->fetchObject();
+}
+catch (PDOException $e) {
+    echo "<script>alert('Virheelline tunnus/salasana!');</script>";
+}
 	if ($kayttaja) {
 		$_SESSION["kayttaja_id"] = $kayttaja->id;
 		$_SESSION["kayttaja"] = $_POST["tunnus"];
