a9fc39361dd0b7683706b1f7fc4ceb9096105641
pturunen <pturunen@ovi.com>
2013-08-30 16:51:22 +0300

    siirretty yhteydenluonti

--- pklisays.php
+++ pklisays.php
@@ -1,18 +1,5 @@
 <?php
-session_start();
-if (!isset($_SESSION["kayttaja"])) {
-    header("Location: eka.html");
-    die();
-}
-//yhteyden luonti tietokantaan
-try {
-    $yhteys = new PDO("pgsql:host=localhost;dbname=pcturune",
-                      "pcturune", "42c747d22fbafe6e");
-} catch (PDOException $e) {
-    die("VIRHE: " . $e->getMessage());
-}
-$yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+require yhteys.php;
 
 //lisayksen tarkistaminen ensin, sitten tapahtumat tulostetaan esille
 //1 tarkista onko ruoka ruokaaine taulussa
